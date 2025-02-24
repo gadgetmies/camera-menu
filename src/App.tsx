993661ab35ec52e5ece5ee584503c017f2bc0f95
Gadgetmies <gadgetmies@gmail.com>
2025-02-24 17:51:25 +0200

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,7 @@
-import {useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {JSX, useState} from 'react'
 import './App.css'
-import * as csvjson from 'csvjson'
-import * as fs from 'fs'
-import * as path from 'path'
 
-const csv = (await import(`./a7iv.csv?raw`)).default;
-
-const menu = [
-    {
-        title: "Photo",
-        children: [
-            {
-                title: "Exposure",
-            }
-        ]
-    }
-]
+import csv from './a7iv.csv?raw'
 
 const process = (acc: Record<string, any>, value: string): void => {
     const commaPosition = value.indexOf(',');
@@ -36,32 +20,72 @@ const process = (acc: Record<string, any>, value: string): void => {
 }
 
 let data = {}
-const [headers, ...rest] = csv.split("\n")
+const [_, ...rest] = csv.split("\n")
 for (const line of rest) {
     process(data, line)
 }
 
 console.log({data})
 
-const render = (node: object, level: number) => {
-    console.log({node})
-    return Object.entries(node).map(([key, value], i) =>
-        <>
-            <div className={`level-${level} container`}>
-                {key}
-            </div>
-            <div style={{position: 'absolute', top: 0}}>
-                {value instanceof Object ? render(value, level + 1) : null}
-            </div>
-        </>
-    )
+type SetSelectedFn = (level: number, index: number) => void
 
+
+const render = (node: Record<string, any>, level: number, selected: Array<number>, setSelected: SetSelectedFn): JSX.Element => {
+    return <>
+        <div className={`level-${level} container`}>
+            {Object.keys(node).map((key, i) =>
+                <div className={`${selected[level] === i ? 'selected' : ''}`}
+                     onClick={() => setSelected(level, i)}>{key}</div>)
+            }
+        </div>
+        {Object.values(node).map((value, index) => value instanceof Object && selected[level] === index ? render(value, level + 1, selected, setSelected) : null)}
+    </>
+}
+
+const setSelected = (update: UpdateFn, memo: Array<number>) => (level: number, index: number) => {
+    let updated = memo.slice()
+    updated.splice(level, updated.length - level, index)
+    update(updated)
+}
+
+type UpdateFn = (memo: Array<number>) => void
+
+const crumbPath = (structure: Record<string, any>, selected: Array<number>) => {
+    const res = selected.reduce(([result, current]: [Array<JSX.Element>, Record<string, any>], index: number): [Array<JSX.Element>, Record<string, any>] => {
+        const currentKey = Object.keys(current)[index];
+        console.log({currentKey})
+        if (!currentKey) {
+            return [result, current]
+        }
+        return [[...result, <div className={'crumb'}>{currentKey}</div>], current[currentKey]];
+    }, [[], structure]);
+    console.log({res})
+    return <div className={'crumb-path'}>{res[0]}</div>
+}
+
+const search = (structure: Record<string, any>, searchString: string, path: Array<number>, select: UpdateFn): JSX.Element[] => {
+    return [...Object.keys(structure)
+        .map((key, index) =>
+            key.toLowerCase().includes(searchString.toLowerCase()) ?
+                <div onClick={() => select([...path, index])}>{key}</div> : null
+        ),
+        ...Object.values(structure).map((value, i) => value instanceof Object ? search(value, searchString, [...path, i], select) : [])
+    ].filter(Boolean) as JSX.Element[]
 }
 
 function App() {
-    const [count, setCount] = useState(0)
-
-    return <div className={'root'}>{render(data, 0)}</div>
+    const [selected, _setSelected] = useState([0, 0])
+    const [searchString, setSearchString] = useState('')
+    const select = setSelected(_setSelected, selected)
+    return <div className={'root'}>
+        {crumbPath(data, selected.length > 2 ? selected.slice(0, -1) : selected)}
+        <div className={'menu'}>{render(data, 0, selected, select)}</div>
+        <div className={'search'}>
+            <label>Search: <input value={searchString} onChange={e => setSearchString(e.target.value)}/></label>
+            <button onClick={() => setSearchString('')}>Clear</button>
+            {searchString !== '' && search(data, searchString, [], _setSelected)}
+        </div>
+    </div>
 }
 
 export default App
