76f94cfe73cf1e9c98af9bb3d62c1b70e2a3d678
Gadgetmies <gadgetmies@gmail.com>
2025-02-24 17:53:15 +0200

    Set base for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,5 @@ import plainText from 'vite-plugin-plain-text';
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), plainText([/\.txt$/]),],
+    base: '/camera-menu/'
 })
